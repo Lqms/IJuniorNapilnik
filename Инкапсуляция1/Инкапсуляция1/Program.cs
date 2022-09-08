62cca2e3c6efc4c81fb675de66a1c7571a6c0d0d
Luqmus <rjcnz1007@yandex.ru>
2022-09-08 09:18:34 +0400

    copypasted task

--- Инкапсуляция1/Инкапсуляция1/Program.cs
+++ Инкапсуляция1/Инкапсуляция1/Program.cs
@@ -10,7 +10,34 @@ namespace Инкапсуляция1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello!");
+            
+        }
+    }
+
+    class Weapon
+    {
+        public int Damage;
+        public int Bullets;
+
+        public void Fire(Player player)
+        {
+            player.Health -= Damage;
+            Bullets -= 1;
+        }
+    }
+
+    class Player
+    {
+        public int Health;
+    }
+
+    class Bot
+    {
+        public Weapon Weapon;
+
+        public void OnSeePlayer(Player player)
+        {
+            Weapon.Fire(player);
         }
     }
 }
